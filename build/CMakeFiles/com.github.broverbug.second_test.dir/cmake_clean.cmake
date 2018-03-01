517072fee892e05865c1a590b47562fbff968ae3
BroverBug <kontakt@brodovski.de>
2018-03-01 21:00:38 +0100

    commit 2

---
+++ build/CMakeFiles/com.github.broverbug.second_test.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "src/hello-again.c"
+  "com.github.broverbug.second_test_valac.stamp"
+  "CMakeFiles/com.github.broverbug.second_test.dir/src/hello-again.c.o"
+  "com.github.broverbug.second_test.pdb"
+  "com.github.broverbug.second_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/com.github.broverbug.second_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
